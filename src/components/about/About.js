c72f6199aaac785c8fb00438c1d4ab1ab45dfcee
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-23 21:45:00 -0700

    Create About component

---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => {
+  return (
+    <div className="about_container">
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default About;
