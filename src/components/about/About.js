e2ca1a7f782447cfe9def121166edbe9f4a163fc
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-24 19:25:05 -0700

    Add key socials links

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,6 +13,29 @@ const About = () => {
           />
           <span>Nelson Yu</span>
         </div>
+        <div className="about_socials">
+          <a
+            href="https://github.com/Nelson-Yu"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/assets/icons/github.png" alt="GitHub Icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nelson-yu/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/assets/icons/linkedin.png" alt="LinkedIn Icon" />
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1DZ0Vi_NIVDOxMrba6KIIRuj4fAG4dhAd/view"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/assets/icons/resume.png" alt="Resume Icon" />
+          </a>
+        </div>
       </div>
       <div className="about_profile_container">
         <p>hello world</p>
