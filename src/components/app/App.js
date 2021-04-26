8236799ee4707c6835d83211c91f3fafb539cd39
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-25 19:22:24 -0700

    Conditionally render HamburgerMenu for mobile devices only

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { isMobileOnly } from 'react-device-detect';
 import About from '../about/About';
 import Chalkboard from '../chalkboard/Chalkboard';
+import HamburgerMenu from '../hamburger-menu/HamburgerMenu';
 import Header from '../header/Header';
 import Navbar from '../navbar/Navbar';
 import './App.scss';
 
 const App = () => {
+  const renderMenu = () => {
+    if (isMobileOnly) {
+      return <HamburgerMenu />;
+    } else {
+      return (
+        <>
+          <Navbar />
+          <Chalkboard />
+        </>
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <Navbar />
-      <Chalkboard />
+      {renderMenu()}
       <Header />
       <About />
     </div>
