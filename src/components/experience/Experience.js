ddbe20905b28511b6e411b5b77d1c5e2a3980707
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-26 22:35:10 -0700

    Create Experience component and stylesheet

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Experience.scss';
+
+const Experience = () => {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default Experience;
