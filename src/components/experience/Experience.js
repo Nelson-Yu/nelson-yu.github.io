55b8552bc2230077e98696e5b600564107df7536
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-27 22:46:16 -0700

    Update Experience component to use vertical timeline

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import EducationItem from '../education-item/EducationItem';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import './Experience.scss';
 
 const Experience = () => {
@@ -8,21 +12,22 @@ const Experience = () => {
       <div className="experience_title">
         <span>Experience</span>
       </div>
-      <div className="experience_education_container">
-        <div className="experience_education_title">
-          <span>Education</span>
-        </div>
-        <div className="experience_education_items">
-          <EducationItem />
-          <EducationItem />
-        </div>
-      </div>
-      <div className="experience_work_container">
-        <div className="experience_work_title">
-          <span>Work</span>
-        </div>
-        <div className="experience_work_items"></div>
-      </div>
+      <VerticalTimeline animate={false}>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   );
 };
