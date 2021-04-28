aa19028ef634d9331b4aa7fa6f1a2c8629b59313
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-27 18:00:42 -0700

    Create education and work item components

---
+++ src/components/education-item/EducationItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EducationItem.scss';
+
+const EducationItem = () => {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default EducationItem;
---
+++ src/components/work-item/WorkItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WorkItem.scss';
+
+const WorkItem = () => {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default WorkItem;
