d4fd95704ae5524fd68fb55b8c4ef704a05587dd
Nelson Yu <nelson.tm.yu@gmail.com>
2021-04-30 16:57:36 -0700

    Create Development Component file

---
+++ src/components/development/Development.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Development.scss';
+
+const Development = () => {
+  return (
+    <div id="development" className="development_container">
+      <div className="development_title">
+        <span>Development</span>
+      </div>
+    </div>
+  );
+};
+
+export default Development;
