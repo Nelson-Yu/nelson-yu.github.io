c7d3dc42100b10d1dda7607e9238dbd197858c9a
Nelson Yu <nelson.tm.yu@gmail.com>
2021-05-02 20:20:10 -0700

    Center project display content

--- src/components/project-display/ProjectDisplay.js
+++ src/components/project-display/ProjectDisplay.js
@@ -4,7 +4,15 @@ import './ProjectDisplay.scss';
 const ProjectDisplay = ({ project }) => {
   return (
     <div className="project_display_container" style={{ color: 'white' }}>
-      <p>{project.title}</p>
+      <div className="project_display_screenshots">
+        <p>{project.title}</p>
+      </div>
+      <div className="project_display_content">
+        <p>
+          {project.title}
+          {project.title}
+        </p>
+      </div>
     </div>
   );
 };
