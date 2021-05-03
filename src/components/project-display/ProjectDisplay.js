f31951fcc773cb7235f28445b4b4e02cf98bd8da
Nelson Yu <nelson.tm.yu@gmail.com>
2021-05-03 00:25:08 -0700

    Add image lightbox modal to display screenshots

--- src/components/project-display/ProjectDisplay.js
+++ src/components/project-display/ProjectDisplay.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+import useDocumentDimensions from '../../hooks/useDocumentDimensions';
+import { isMobileOnly } from 'react-device-detect';
 import './ProjectDisplay.scss';
+import 'react-image-lightbox/style.css';
 
 const ProjectDisplay = ({ project }) => {
+  const { width } = useDocumentDimensions();
+  useDocumentDimensions;
+  const [openLightbox, setOpenLightbox] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
+  const handleOpenLightbox = (index) => {
+    setPhotoIndex(index);
+    setOpenLightbox(true);
+  };
+
   return (
     <div className="project_display_container" style={{ color: 'white' }}>
       <div className="project_display_content">
@@ -15,6 +29,14 @@ const ProjectDisplay = ({ project }) => {
           <div
             key={index}
             className={`project_display_polaroid project_display_polaroid_${index}`}
+            onClick={() => {
+              handleOpenLightbox(index);
+            }}
+            onKeyDown={() => {
+              handleOpenLightbox(index);
+            }}
+            role="button"
+            tabIndex={0}
           >
             <img
               src={`/assets/screenshots/${screenshot}`}
@@ -24,6 +46,35 @@ const ProjectDisplay = ({ project }) => {
           </div>
         ))}
       </div>
+
+      {openLightbox && project.screenshots.length > 1 && (
+        <Lightbox
+          mainSrc={`/assets/screenshots/${project.screenshots[photoIndex]}`}
+          nextSrc={`/assets/screenshots/${
+            project.screenshots[(photoIndex + 1) % project.screenshots.length]
+          }`}
+          prevSrc={`/assets/screenshots/${
+            project.screenshots[
+              (photoIndex + project.screenshots.length - 1) %
+                project.screenshots.length
+            ]
+          }`}
+          onCloseRequest={() => {
+            setOpenLightbox(false);
+          }}
+          onMovePrevRequest={() =>
+            setPhotoIndex(
+              (photoIndex + project.screenshots.length - 1) %
+                project.screenshots.length
+            )
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % project.screenshots.length)
+          }
+          imagePadding={width < 768 || isMobileOnly ? 50 : 100}
+          reactModalStyle={{ zIndex: 1500 }}
+        />
+      )}
     </div>
   );
 };
