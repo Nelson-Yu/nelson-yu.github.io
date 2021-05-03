5051d8e0c9a7d46cc3cbd418c25898cd1fc121af
Nelson Yu <nelson.tm.yu@gmail.com>
2021-05-02 22:26:28 -0700

    Add screenshots to project display view

--- src/components/project-display/ProjectDisplay.js
+++ src/components/project-display/ProjectDisplay.js
@@ -4,14 +4,25 @@ import './ProjectDisplay.scss';
 const ProjectDisplay = ({ project }) => {
   return (
     <div className="project_display_container" style={{ color: 'white' }}>
-      <div className="project_display_screenshots">
-        <p>{project.title}</p>
-      </div>
       <div className="project_display_content">
         <p>
           {project.title}
           {project.title}
         </p>
+      </div>
+      <div className="project_display_screenshots">
+        {project.screenshots.map((screenshot, index) => (
+          <div
+            key={index}
+            className={`project_display_polaroid project_display_polaroid_${index}`}
+          >
+            <img
+              src={`/assets/screenshots/${screenshot}`}
+              alt={`screenshot_${index}`}
+              className="project_display_image"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
