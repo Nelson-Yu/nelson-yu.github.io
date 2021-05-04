fb7f77c99d36236c3d5f394e208762d6dd7e0eb9
Nelson Yu <nelson.tm.yu@gmail.com>
2021-05-03 22:17:38 -0700

    Update SVG dimensions

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,14 @@ const projectsData = [
     role: 'App Architect, Full-Stack Developer',
     description:
       'LivePlanBe+ is a Digital Lab developed application in partnership with Painc BC. The goal is to enhance the well-being of all people living with pain through personalized support, recommendations and learning modules. This project is currently in development and is being built with a variation of the MERN stack, substituting ExpressJS with KoaJS, and GraophQL.',
-    stack: ['MongoDB', 'KoaJS', 'NodeJS', 'React', 'GraphQL', 'Docker'],
+    stack: [
+      'mongodb.svg',
+      'koa.svg',
+      'nodejs.svg',
+      'react.svg',
+      'graphql.svg',
+      'docker.svg',
+    ],
     screenshots: ['wip.png'],
   },
   {
@@ -15,13 +22,13 @@ const projectsData = [
     description:
       "CDR is a Digital Lab redesign and update of a joint project between Sunny Hill Health Centre at BC Children's Hospital and the BC Ministry of Children and Family Development. The objective is to provide a platform for users to search for resources of their interest regarding to children, youth, and family health. This project is currently in development and is being built with a variation of the MERN stack, substituting ExpressJS with KoaJS, and GraphQL.",
     stack: [
-      'MongoDB',
-      'KoaJS',
-      'NodeJS',
-      'React',
-      'GraphQL',
-      'Docker',
-      'Algolia',
+      'mongodb.svg',
+      'koa.svg',
+      'nodejs.svg',
+      'react.svg',
+      'graphql.svg',
+      'docker.svg',
+      'algolia.svg',
     ],
     screenshots: ['wip.png'],
   },
@@ -31,7 +38,7 @@ const projectsData = [
     role: 'Primary Full-Stack Developer',
     description:
       "Early Childhood Outside, a Digital Lab developed project is an extension of an existing project called Outside Play. The overall goal of the project is to promote and educate caregivers and early childhood educators to embrace the many benefits that outdoor play can have on a child's growth. This is done through a variety of interactive videos and questionnaires to craft an Action Plan to for the participant to act on. This project was built utilizing MySQL, Laravel(PHP), and VueJS.",
-    stack: ['MySQL', 'Laravel', 'Vue'],
+    stack: ['mysql.svg', 'laravel.svg', 'vue.svg'],
     screenshots: ['eco-1.png', 'eco-2.png', 'eco-3.png'],
     link: 'https://eco.outsideplay.ca/en/pages/chapter-1-intro',
   },
@@ -41,7 +48,7 @@ const projectsData = [
     role: 'Primary Developer',
     description:
       'CSKT is a Digital Lab platform with the goal of teaching youth and families that they have the opportunity to participate in research, along with how that can directly impact lives of others. Users of the app are prompted to follow along an interactive journey, built with jQuery within Adobe Animate.',
-    stack: ['Adobe Animate', 'jQuery'],
+    stack: ['animate.svg', 'jquery.svg'],
     screenshots: ['cskt-1.png', 'cskt-2.png', 'cskt-3.png'],
     link: 'https://cskt.bcchdigital.ca/',
   },
@@ -51,7 +58,7 @@ const projectsData = [
     role: 'Full-Stack Developer',
     description:
       "Activity Reporting is a Digital Lab application designed for internal use by the physicians at BC Children's Hospital Department of Pediatrics. This project allows physicians to track their annual activities within the hospital and out, while the Department of Pediatrics conducts internal data analysis. This app is built with Laravel as the back-end and React for the front-end.",
-    stack: ['MySQL', 'Laravel', 'React'],
+    stack: ['mysql.svg', 'laravel.svg', 'react.svg'],
     screenshots: ['ar-1.png', 'ar-2.png', 'ar-3.png'],
   },
 ];
