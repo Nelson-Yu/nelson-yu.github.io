690279561e1421c8449a43a472e1586cb9599ecb
Nelson Yu <nelson.tm.yu@gmail.com>
2021-05-10 22:38:51 -0700

    Update content body of project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,7 +20,7 @@ const projectsData = [
     date: 'Mar 2021',
     role: 'App Architect, Full-Stack Developer',
     description:
-      "CDR is a Digital Lab redesign and update of a joint project between Sunny Hill Health Centre at BC Children's Hospital and the BC Ministry of Children and Family Development. The objective is to provide a platform for users to search for resources of their interest regarding to children, youth, and family health. This project is currently in development and is being built with a variation of the MERN stack, substituting ExpressJS with KoaJS, and GraphQL.",
+      "CDR is a Digital Lab redesign and update of a joint project between Sunny Hill Health Centre at BC Children's Hospital and the BC Ministry of Children and Family Development. The objective is to provide a platform for users to search for children, youth, and family health resources that are relevant to them. This project is currently in development and is being built with a variation of the MERN stack, substituting ExpressJS with KoaJS, and GraphQL.",
     stack: [
       'mongodb.svg',
       'koa.svg',
@@ -37,7 +37,7 @@ const projectsData = [
     date: 'Dec 2020',
     role: 'Primary Full-Stack Developer',
     description:
-      "Early Childhood Outside, a Digital Lab developed project is an extension of an existing project called Outside Play. The overall goal of the project is to promote and educate caregivers and early childhood educators to embrace the many benefits that outdoor play can have on a child's growth. This is done through a variety of interactive videos and questionnaires to craft an Action Plan to for the participant to act on. This project was built utilizing MySQL, Laravel(PHP), and VueJS.",
+      "A Digital Lab developed project acting as an extension of an existing project called Outside Play. The overall goal of the project is to promote and educate caregivers and early childhood educators to embrace the many benefits that outdoor play can have on a child's growth. This is done through a variety of interactive videos and questionnaires to craft an Action Plan to for the participant to act on. This project was built utilizing MySQL, Laravel(PHP), and VueJS.",
     stack: ['mysql.svg', 'laravel.svg', 'vue.svg'],
     screenshots: ['eco-1.png', 'eco-2.png', 'eco-3.png'],
     link: 'eco.outsideplay.ca',
@@ -47,7 +47,7 @@ const projectsData = [
     date: 'May 2020',
     role: 'Primary Developer',
     description:
-      'CSKT is a Digital Lab platform with the goal of teaching youth and families that they have the opportunity to participate in research, along with how that can directly impact lives of others. Users of the app are prompted to follow along an interactive journey, built with jQuery within Adobe Animate.',
+      'CSKT is a Digital Lab platform with the goal of encouraging youth and families to participate in research opportunities at BC Children’s Hospital. The project takes the user on an interactive journey to learn about how participation can directly impact lives of others in a positive manner. This app was built with jQuery within Adobe Animate.',
     stack: ['animate.svg', 'jquery.svg'],
     screenshots: ['cskt-1.png', 'cskt-2.png', 'cskt-3.png'],
     link: 'cskt.bcchdigital.ca',
@@ -57,7 +57,7 @@ const projectsData = [
     date: 'Jan 2020',
     role: 'Full-Stack Developer',
     description:
-      "Activity Reporting is a Digital Lab application designed for internal use by the physicians at BC Children's Hospital Department of Pediatrics. This project allows physicians to track their annual activities within the hospital and out, while the Department of Pediatrics conducts internal data analysis. This app is built with Laravel as the back-end and React for the front-end.",
+      "Activity Reporting is a Digital Lab application designed for internal use by the physicians at BC Children's Hospital Department of Pediatrics. This project allows physicians to track their annual activities within the hospital and out, while the Department of Pediatrics conducts internal data analysis. This app was built with Laravel as the back-end and React for the front-end.",
     stack: ['mysql.svg', 'laravel.svg', 'react.svg'],
     screenshots: ['ar-1.png', 'ar-2.png', 'ar-3.png'],
   },
